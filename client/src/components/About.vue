2972664a02ac28c3e7b477a862cbb249d565ddf2
abdi <abdivhhf@gmail.com>
2019-11-26 17:49:24 -0600

    complete lab

---
+++ client/src/components/About.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="about">
+
+        <h2> Where have you ever traveled?</h2>
+
+        <p> Use this site to keep track of how many states you have visited</p>
+        <p> A Vue.js site by Abdi. </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ABout'
+}
+</script>
---
+++ client/vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    devServer: {
+        proxy: 'http://127.0.0.1:3000',
+
+            overlay: {
+                warnings: false,
+                errors: false
+            }
+    }
+}
